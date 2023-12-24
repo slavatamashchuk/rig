2733ee32c4016a054595266f9c218dfc7dd1eed5
Slava Tamashchuk <slava.tamashchuk@gmail.com>
2023-12-24 20:45:37 +0200

    Optimized generating

--- main.py
+++ main.py
@@ -12,27 +12,21 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Window")
 surface = pygame.Surface((width, height))
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+for y in range(height):
+    for x in range(width):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
 
-    surface.set_at((x, y), (r, g, b))
+        surface.set_at((x, y), (r, g, b))
+
     screen.blit(surface, (0, 0))
     pygame.display.flip()
-
-    if x == width and y == height:
-        break
-    elif x <= width:
-        x += 1
-    else:
-        y += 1
-        x = 0
 
 pygame.image.save(screen, name)
 print(f"\nRandom image saved as {name}.")
